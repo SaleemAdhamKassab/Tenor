dab61f38347bf410f51bd241ffbcc68aad3ade01
MohamadIsmail95 <mhdismail725@gmail.com>
2023-11-28 14:47:25 +0300

    correct enum element

--- Helper/Constant.cs
+++ Helper/Constant.cs
@@ -3,6 +3,6 @@
     public class Constant
     {
         public enum enOPerationTypes { counter, function, kpi, opt, voidFunction }
-        public enum enAggregation { min, mix, sum, avg, count }
+        public enum enAggregation { min, max, sum, avg, count }
     }
 }
