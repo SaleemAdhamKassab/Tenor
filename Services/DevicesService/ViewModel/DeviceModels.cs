2079139d53e2ed467bce043de0c3610ac0ac5e00
saleem <e.s.a.kassab@gmail.com>
2023-12-06 07:41:55 +0300

    update device models

---
+++ Services/DevicesService/ViewModel/DeviceModels.cs
@@ -0,0 +1,43 @@
+﻿using Tenor.Dtos;
+using Tenor.Models;
+
+namespace Tenor.Services.DevicesService.ViewModels
+{
+    public class DeviceViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDeleted { get; set; }
+        public string SupplierId { get; set; }
+
+        //navigation properties
+        public int? ParentId { get; set; }
+        public string ParentName { get; set; }
+
+        public List<Subset> Subsets { get; set; }
+    }
+
+    public class DeviceListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class DeviceBindingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsDeleted { get; set; }
+        public string SupplierId { get; set; }
+        public int? ParentId { get; set; }
+    }
+
+    public class DeviceFilterModel : GeneralFilterModel
+    {
+        public string Name { get; set; }
+    }
+}
--- Services/DevicesService/ViewModels/DeviceFilterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tenor.Dtos;
-
-namespace Tenor.Services.DevicesService.ViewModels
-{
-    public class DeviceFilterModel : GeneralFilterModel
-    {
-        public string Name { get; set; }
-    }
-}
