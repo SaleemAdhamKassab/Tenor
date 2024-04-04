0aeb293d74c7c0fcae9b4769b35ccb846d1f03bf
Mhd Ismail <Mhd Ismail@mhdismail725>
2024-04-04 10:20:36 +0300

    edit report service

--- Services/ReportService/ReportService.cs
+++ Services/ReportService/ReportService.cs
@@ -41,7 +41,6 @@ namespace Tenor.Services.ReportService
             {
                 return new ResultWithMessage(null, "Access Denied");
             }
-
             if (_sharedService.IsExist(0,input.DeviceId,null,null,input.Name))
             {
                 return new ResultWithMessage(null, "This Report name alraedy exsit on the same device");
@@ -64,8 +63,7 @@ namespace Tenor.Services.ReportService
                     //-----------------Create Report-----------------------
                     Report report = new Report(input);
                     report.ChildId= (report.ChildId==0 || report.ChildId==null) ? null:report.ChildId;
-                    _db.Reports.Add(report);
-                    _db.SaveChanges();
+                    report.Measures = new List<ReportMeasure>();
                     if (input.ReportFields != null)
                     {
                         bool isCorrectSave = _sharedService.AddExtraFields(null,(int) report.Id, input.ReportFields);
@@ -75,23 +73,24 @@ namespace Tenor.Services.ReportService
                         }
                     }
                     //-----------------Create Report Levels----------------
-                    foreach(ReportLevelDto s in input.Levels)
+                    report.Levels = input.Levels.Select(x=> new ReportLevel()
                     {
-                        ReportLevel reportLevel = new ReportLevel(s.Id,s.DisplayOrder,s.SortDirection,report.Id,s.DimensionLevelId);
-                        _db.ReportLevels.Add(reportLevel);
-                        _db.SaveChanges();
+                        Id=x.Id,
+                        DisplayOrder=x.DisplayOrder,
+                        SortDirection=x.SortDirection,
+                        ReportId=report.Id,
+                        DimensionLevelId=x.DimensionLevelId
 
-                    }
+                    }).ToList();
                     //-----------------Create Report Filters--------------
-                    foreach (ReportFilterDto r in input.Filters)
+                    report.Filters = input.Filters.Select(x => new ReportFilter()
                     {
-
-                        ReportFilter reportFilter = new ReportFilter(r.Id,r.LogicalOperator,_sharedService.ConvertListToString(r.Value),report.Id,r.DimensionLevelId);
-                        _db.ReportFilters.Add(reportFilter);
-                        _db.SaveChanges();
-
-
-                    }
+                        Id=x.Id,
+                        LogicalOperator=x.LogicalOperator,
+                        Value= _sharedService.ConvertListToString(x.Value),
+                        ReportId= report.Id,
+                        DimensionLevelId=x.DimensionLevelId
+                    }).ToList();
                     //-----------------Create Report Measures-------------
                     foreach (ReportMeasureDto m in input.Measures)
                     {
@@ -104,16 +103,14 @@ namespace Tenor.Services.ReportService
                         {
                             return new ResultWithMessage(null,_sharedService.CheckValidFormat(m.Operation).Message);
                         }
-
+                        
                         ReportMeasure measure = _mapper.Map<ReportMeasure>(m);
                         measure.ReportId = report.Id;
-                        _db.ReportMeasures.Add(measure);
-                        _db.SaveChanges();
-
-                        //----------------- Havings--------------
+                        report.Measures.Add(measure);
 
                     }
-
+                    _db.Reports.Add(report);
+                    _db.SaveChanges();
                     transaction.Complete();
                     return new ResultWithMessage(report, null);
 
