57e6e491b71d38f5a639d895dbfb48b9a105ab98
Administrator <Administrator@DESKTOP-IF8JJLJ>
2024-04-24 15:45:14 +0300

    Add getDimensionLevels

--- Services/ReportService/ReportService.cs
+++ Services/ReportService/ReportService.cs
@@ -13,126 +13,129 @@ using AutoMapper;
 
 namespace Tenor.Services.ReportService
 {
-    public interface IReportService
-    {
-        Task<ResultWithMessage> Add(CreateReport input, TenantDto authUser);
+	public interface IReportService
+	{
+		Task<ResultWithMessage> Add(CreateReport input, TenantDto authUser);
+		Task<ResultWithMessage> getDimensionLevels(List<ReportMeasure> measures);
+	}
+	public class ReportService : IReportService
+	{
+		private readonly TenorDbContext _db;
+		private readonly IJwtService _jwtService;
+		private readonly ISharedService _sharedService;
+		private readonly IMapper _mapper;
 
-    }
-    public class ReportService : IReportService
-    {
-        private readonly TenorDbContext _db;
-        private readonly IJwtService _jwtService;
-        private readonly ISharedService _sharedService;
-        private readonly IMapper _mapper;
+		public ReportService(TenorDbContext db, IJwtService jwtService, ISharedService sharedService, IMapper mapper)
+		{
+			_db = db;
+			_jwtService = jwtService;
+			_sharedService = sharedService;
+			_mapper = mapper;
+		}
 
-        public ReportService(TenorDbContext db, IJwtService jwtService, ISharedService sharedService, IMapper mapper)
-        {
-            _db=db;
-            _jwtService=jwtService;
-            _sharedService=sharedService;
-            _mapper=mapper;
-        }
+		public async Task<ResultWithMessage> Add(CreateReport input, TenantDto authUser)
+		{
+			//---------------------General Validations---------------------------------
+			string accessResult = _jwtService.checkUserTenantPermission(authUser, input.DeviceId);
+			if (accessResult == enAccessType.denied.GetDisplayName())
+			{
+				return new ResultWithMessage(null, "Access Denied");
+			}
+			if (_sharedService.IsExist(0, input.DeviceId, null, null, input.Name))
+			{
+				return new ResultWithMessage(null, "This Report name alraedy exsit on the same device");
 
-        public async Task<ResultWithMessage> Add(CreateReport input, TenantDto authUser)
-        {
-            //---------------------General Validations---------------------------------
-            string accessResult = _jwtService.checkUserTenantPermission(authUser, input.DeviceId);
-            if (accessResult == enAccessType.denied.GetDisplayName())
-            {
-                return new ResultWithMessage(null, "Access Denied");
-            }
-            if (_sharedService.IsExist(0,input.DeviceId,null,null,input.Name))
-            {
-                return new ResultWithMessage(null, "This Report name alraedy exsit on the same device");
+			}
+			foreach (var s in input.Measures)
+			{
+				if (Convert.ToBoolean(_sharedService.CheckValidFormat(s.Operation).Data) == false)
+				{
+					return new ResultWithMessage(null, _sharedService.CheckValidFormat(s.Operation).Message);
 
-            }
-            foreach(var s in input.Measures)
-            {
-                if (Convert.ToBoolean(_sharedService.CheckValidFormat(s.Operation).Data) == false)
-                {
-                    return new ResultWithMessage(null, _sharedService.CheckValidFormat(s.Operation).Message);
+				}
 
-                }
+			}
 
-            }
+			using (TransactionScope transaction = new TransactionScope())
+			{
+				try
+				{
+					//-----------------Create Report-----------------------
+					Report report = new Report(input);
+					report.ChildId = (report.ChildId == 0 || report.ChildId == null) ? null : report.ChildId;
+					report.Measures = new List<ReportMeasure>();
+					if (input.ReportFields != null)
+					{
+						bool isCorrectSave = _sharedService.AddExtraFields(null, (int)report.Id, input.ReportFields);
+						if (!isCorrectSave)
+						{
+							return new ResultWithMessage(null, "Mandatory Field error");
+						}
+					}
+					//-----------------Create Report Levels----------------
+					report.Levels = input.Levels.Select(x => new ReportLevel()
+					{
+						Id = x.Id,
+						DisplayOrder = x.DisplayOrder,
+						SortDirection = x.SortDirection,
+						ReportId = report.Id,
+						DimensionLevelId = x.DimensionLevelId
 
-            using (TransactionScope transaction = new TransactionScope())
-            {
-                try
-                {
-                    //-----------------Create Report-----------------------
-                    Report report = new Report(input);
-                    report.ChildId= (report.ChildId==0 || report.ChildId==null) ? null:report.ChildId;
-                    report.Measures = new List<ReportMeasure>();
-                    if (input.ReportFields != null)
-                    {
-                        bool isCorrectSave = _sharedService.AddExtraFields(null,(int) report.Id, input.ReportFields);
-                        if (!isCorrectSave)
-                        {
-                            return new ResultWithMessage(null, "Mandatory Field error");
-                        }
-                    }
-                    //-----------------Create Report Levels----------------
-                    report.Levels = input.Levels.Select(x=> new ReportLevel()
-                    {
-                        Id=x.Id,
-                        DisplayOrder=x.DisplayOrder,
-                        SortDirection=x.SortDirection,
-                        ReportId=report.Id,
-                        DimensionLevelId=x.DimensionLevelId
+					}).ToList();
+					//-----------------Create Report Filters--------------
+					report.FilterContainers = input.ContainerOfFilters.Select(x => new ReportFilterContainer()
+					{
+						Id = x.Id,
+						ReportId = report.Id,
+						ReportFilters = x.ReportFilters.Select(y => new ReportFilter()
+						{
+							Id = y.Id,
+							LogicalOperator = y.LogicalOperator,
+							Value = _sharedService.ConvertListToString(y.Value),
+							FilterContainerId = x.Id,
+							DimensionLevelId = y.DimensionLevelId
+						}).ToList()
 
-                    }).ToList();
-                    //-----------------Create Report Filters--------------
-                    report.FilterContainers = input.ContainerOfFilters.Select(x => new ReportFilterContainer()
-                    {
-                        Id=x.Id,
-                        ReportId= report.Id,
-                        ReportFilters= x.ReportFilters.Select(y=> new ReportFilter()
-                        {
-                            Id = y.Id,
-                            LogicalOperator = y.LogicalOperator,
-                            Value = _sharedService.ConvertListToString(y.Value),
-                            FilterContainerId = x.Id,
-                            DimensionLevelId = y.DimensionLevelId
-                        }).ToList()
-
-                }).ToList();
+					}).ToList();
 
 
-                    //-----------------Create Report Measures-------------
-                    foreach (ReportMeasureDto m in input.Measures)
-                    {
-                        if (_sharedService.IsExist(0, null, null, m.DisplayName, null))
-                        {
-                            return new ResultWithMessage(null, "This Measure name :  "+m.DisplayName+"  alraedy exsit");
+					//-----------------Create Report Measures-------------
+					foreach (ReportMeasureDto m in input.Measures)
+					{
+						if (_sharedService.IsExist(0, null, null, m.DisplayName, null))
+						{
+							return new ResultWithMessage(null, "This Measure name :  " + m.DisplayName + "  alraedy exsit");
 
-                        }
-                        if (Convert.ToBoolean(_sharedService.CheckValidFormat(m.Operation).Data) == false)
-                        {
-                            return new ResultWithMessage(null,_sharedService.CheckValidFormat(m.Operation).Message);
-                        }
-                        
-                        ReportMeasure measure = _mapper.Map<ReportMeasure>(m);
-                        measure.ReportId = report.Id;
-                        report.Measures.Add(measure);
+						}
+						if (Convert.ToBoolean(_sharedService.CheckValidFormat(m.Operation).Data) == false)
+						{
+							return new ResultWithMessage(null, _sharedService.CheckValidFormat(m.Operation).Message);
+						}
 
-                    }
-                    _db.Reports.Add(report);
-                    _db.SaveChanges();
-                    transaction.Complete();
-                    return new ResultWithMessage(report, null);
+						ReportMeasure measure = _mapper.Map<ReportMeasure>(m);
+						measure.ReportId = report.Id;
+						report.Measures.Add(measure);
 
-                }
-                catch (Exception ex)
-                {
-                    return new ResultWithMessage(null, ex.Message);
+					}
+					_db.Reports.Add(report);
+					_db.SaveChanges();
+					transaction.Complete();
+					return new ResultWithMessage(report, null);
 
-                }
-            }
-           
+				}
+				catch (Exception ex)
+				{
+					return new ResultWithMessage(null, ex.Message);
 
-        }
+				}
+			}
 
-        
-    }
+
+		}
+
+		public async Task<ResultWithMessage> getDimensionLevels(List<ReportMeasure> measures)
+		{
+			return new ResultWithMessage(null, string.Empty);
+		}
+	}
 }
